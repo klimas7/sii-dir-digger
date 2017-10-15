6d7ca4d8ea245b6adcf64e598e2a0dbd79590484
klimas7 <klimas7@gmail.com>
2017-10-15 13:00:59 +0200

    DirDiggerDefinition skeleton

--- src/main/java/org/jenkinsci/plugins/dirdigger/DirDiggerDefinition.java
+++ src/main/java/org/jenkinsci/plugins/dirdigger/DirDiggerDefinition.java
@@ -1,4 +1,34 @@
 package org.jenkinsci.plugins.dirdigger;
 
-public class DirDiggerDefinition {
+import hudson.Extension;
+import hudson.model.ParameterDefinition;
+import hudson.model.ParameterValue;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+public class DirDiggerDefinition extends ParameterDefinition {
+
+    @DataBoundConstructor
+    public DirDiggerDefinition(String name, String description) {
+        super(name, description);
+    }
+
+    @Override
+    public ParameterValue createValue(StaplerRequest req, JSONObject jo) {
+        return null;
+    }
+
+    @Override
+    public ParameterValue createValue(StaplerRequest req) {
+        return null;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends ParameterDescriptor {
+        @Override
+        public String getDisplayName() {
+            return "Dir Digger";
+        }
+    }
 }
